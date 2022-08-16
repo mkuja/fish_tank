33b1046270c8bfeedc83f9da093abd470d541ec5
Mikko Kujala <m.kujala@live.com>
2022-08-16 22:19:40 +0300

    jee

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module websAsciiquarium
 
 go 1.19
 
-require github.com/gorilla/websocket v1.5.0 // indirect
+require (
+	github.com/creack/pty v1.1.18 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"bytes"
+	"fmt"
+	"github.com/creack/pty"
 	"github.com/gorilla/websocket"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
 	"time"
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1920,
-	WriteBufferSize: 1920,
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
@@ -33,31 +36,63 @@ func aquarium(writer http.ResponseWriter, request *http.Request) {
 	go spawnAquarium(ws)
 }
 
-func spawnAquarium(ws *websocket.Conn) {
-	log.Println("Spawning aquarium..")
-	cmd := exec.Command("/usr/bin/asciiquarium")
-	buf := bytes.Buffer{}
-	cmd.Stdout = &buf
-
-	err := cmd.Start()
+func pError(err *error) bool {
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
+		return true
+	}
+	return false
+}
+
+func sendToClient(ws *websocket.Conn, data []byte) (int, error) {
+	writer, err := ws.NextWriter(websocket.TextMessage)
+	if err != nil {
+		pError(&err)
+		return 0, err
+	}
+	sent, err := writer.Write(data)
+	if err != nil {
+		pError(&err)
+		return 0, err
+	}
+	writer.Close()
+	return sent, nil
+}
+
+func readFromClient(ws *websocket.Conn, buf []byte) (int, error) {
+	_, reader, err := ws.NextReader()
+	if err != nil {
+		pError(&err)
+		return 0, err
+	}
+	read, err := reader.Read(buf)
+	if err != nil {
+		return 0, err
+	}
+	return read, nil
+}
+
+func spawnAquarium(ws *websocket.Conn) {
+	fmt.Fprintf(os.Stderr, "Spawning asciiquarium\n")
+	c := exec.Command("asciiquarium")
+	p, err0 := pty.Start(c)
+	defer c.Process.Kill()
+	if err0 != nil {
+		pError(&err0)
 		return
 	}
+
 	for {
-		time.Sleep(50 * time.Millisecond)
-		tx, txErr := ws.NextWriter(websocket.TextMessage)
-		// Open handle for writer
-		if txErr != nil {
-			// Connection must have been closed.
-			log.Println(txErr)
-			log.Println("Waiting to asciiquarium to shut down...")
-			cmd.Wait()
-			log.Println("asciiquarium shut down.")
+		buf := make([]byte, 1024)
+		read, err := p.Read(buf)
+		if err == io.EOF {
 			return
 		}
-		tx.Write(buf.Bytes())
-		tx.Close()
+		_, err = sendToClient(ws, buf[:read])
+		if err != nil {
+			return
+		}
+		time.Sleep(time.Millisecond * 20)
 	}
 }
 
@@ -70,4 +105,3 @@ func main() {
 		return
 	}
 }
-::
